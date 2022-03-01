c09ad69fecd9f4214ce83de5a0c1a9073e53bf66
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2022-03-01 22:00:36 +0530

    Edit View Implemented

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -72,5 +72,19 @@ namespace EmployeeManagement.Controllers
             }
             return View();
         }
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            EmployeeEditViewModel employeeEditViewModel = new EmployeeEditViewModel()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistingPhotoPath = employee.PhotoPath
+            };
+            return View(employeeEditViewModel);
+        }
     }
 }
