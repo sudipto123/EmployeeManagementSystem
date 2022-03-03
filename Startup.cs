ee4f636affd0569a9f1a536a3bff6cdeda6aee99
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2022-03-03 18:35:20 +0530

    Password options

--- Startup.cs
+++ Startup.cs
@@ -30,8 +30,12 @@ namespace EmployeeManagement
             services.AddDbContextPool<AppDbContext>(
                 options => options.UseSqlServer(_configuration.GetConnectionString("EmployeeDBConnection")));
 
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                        .AddEntityFrameworkStores<AppDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>(
+                options =>
+                {
+                    options.Password.RequiredLength = 10;
+                    options.Password.RequiredUniqueChars = 3;
+                }).AddEntityFrameworkStores<AppDbContext>();            
 
             services.AddMvc(option => option.EnableEndpointRouting = false).AddXmlDataContractSerializerFormatters();
             
